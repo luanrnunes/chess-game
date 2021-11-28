4564ad89a83ece6ad99694050dc5b46adc0d31a4
luanrnunes <luanrobertonunes@hotmail.com>
2021-11-28 18:43:44 -0300

    Exceptions

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,6 +8,9 @@ public class Board {
 	
 	public Board(int rows, int columns) {
 		
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Erro ao criar o tabuleiro, necessario pelo menos uma linha e uma coluna");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -17,31 +20,53 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
 
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
 	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Posicao nao existente no tabuleiro");
+		}
 		return pieces[row][column];
 	}
 	
 	/*Sobrecarga*/
 	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao nao existente no tabuleiro");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	/*Pega a matriz declarada no inicio do programa, na posicao dada e atribuir a ela a peça que eu informei*/
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("Ja existe uma peca na posicao " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+		
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+	
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao nao existente no tabuleiro");
+		}
+		
+		return piece(position) != null;
 	}
 
 }
