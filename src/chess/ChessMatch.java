262b492888e9126546e41a5dc2d76963d23991fa
luanrnunes <luanrobertonunes@hotmail.com>
2021-11-29 23:54:32 -0300

    Novas Exceptions e posicoes do tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,11 +26,15 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	/*new position aponta para linha 2 (conte 0,1,2 na vertical) e coluna 1(conte 0,1 na horizontal)*/
 	private void initialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position (2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e',1, new King(board, Color.WHITE));
 		
 	}
 
