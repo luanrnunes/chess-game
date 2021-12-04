818ed9e81e62aa21dbde3753050c4f40ef135ba6
luanrnunes <luanrobertonunes@hotmail.com>
2021-12-04 16:47:26 -0300

    Movimentos da torre

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -24,6 +24,12 @@ public class Position {
 		this.column = column;
 	}
 	
+	//above
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+			}
+	
 	@Override
 	public String toString()
 	{
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
@@ -46,6 +47,12 @@ public class ChessMatch {
 		}
 		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("Nao existem movimentos disponiveis para esta peca");
+		}
+	}
+	
+	private void validateTargetPosition(Position source, Position Target) {
+		if (!board.piece(source).possibleMove(Target)) {
+			throw new ChessException("A peca selecionada nao pode se mover ao desino selecionado");
 		}
 	}
 	
